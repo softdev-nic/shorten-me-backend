59f61b62b5af33c21509bc06080bb9d6b7fe80b7
softdev-nic <softtechon.inc@gmail.com>
2026-05-15 21:22:09 +0530

    Revert "shortner api ready"
    This reverts commit 7c4b3df4109266e9ea82edd0878d50843b3035c3.

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const app = express();
-const db = require('./db')
-const Shortlinks = require('./View/Shortlinks')
-const CreateLink = require('./View/Controller/createLink')
-const searchById = require('./View/Controller/searchById')
-const cors = require('cors')
-require('dotenv').config();
 const port = 3000
-
-app.use(cors())
-app.use(express.json());
-app.post('/createlink', CreateLink);
-app.get('/:id', searchById)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-
 })
+
